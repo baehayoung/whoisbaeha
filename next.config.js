3b584894d12664d5caabeb3c802bd1aaf0b5fc6f
baehayoung <baehayoung214@gmail.com>
2023-11-21 00:48:19 +0900

    Modify some setting files
    :Detailed Notes:
    1) Add images option in next config
    2) Change output dir in gitignore

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const isProd = process.env.NODE_ENV === 'production'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: "/whoisbaeha",
+    output: "export",
+    images: { unoptimized: true }
 };
 
 module.exports = nextConfig
