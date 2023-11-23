26fbc7d806c27d47ca060bd9478245ecc593cf88
baehayoung <baehayoung214@gmail.com>
2023-11-24 01:01:25 +0900

    Change font and remove some css value
    :Detailed Notes:
    1) Change home page font
    2) Remove unnecessary css value

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       </div>
       <div className="firstPparagraph">
         <h2>안녕하세요.</h2>
-        <h1>프론트엔드 개발자 배하입니다.</h1>
+        <h1>프론트엔드 개발자 BAEHA입니다.</h1>
         <h2>만든 것을 바로바로 눈으로 확인하는 것이 좋아서 FE 개발을 시작했습니다</h2>
         <h2>다양한 피드백을 받고,그 피드백을 바탕으로 성장하는 그런 개발자가 되고싶습니다.</h2>
       </div>
